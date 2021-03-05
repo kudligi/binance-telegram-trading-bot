f0237c436658995d8abe0a14288d6fc5e7f8d122
nihal kudligi <nihal1011ML@gmail.com>
2021-03-05 23:15:06 +0530

    added import json

--- telegram/views.py
+++ telegram/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.views.decorators.http import require_POST
 
 from django.http import HttpResponse
- 
+import json
 
 # Create your views here.
 def talkin_to_me_bruh(request):
     # please insert magic here
     req = json.loads(request.body)
-    print(req)
+    print("Telegram request was ", req)
     return HttpResponse('OK')
 
 
