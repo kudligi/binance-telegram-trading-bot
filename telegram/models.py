e0b630ab8ad610901507ae4c81e2974f2ac7dd71
nihal kudligi <nihal1011ML@gmail.com>
2021-03-06 01:41:16 +0530

    added telegram user model

--- telegram/models.py
+++ telegram/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    chat_id = models.IntegerField()
+    username = models.CharField(max_length=25)
+    knows = models.BooleanField()
