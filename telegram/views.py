c5496860f92f2e6981b047ded1f41b7024628277
nihal kudligi <nihal1011ML@gmail.com>
2021-03-06 02:14:47 +0530

    added user registry

--- telegram/views.py
+++ telegram/views.py
@@ -15,31 +15,46 @@ def talkin_to_me_bruh(request):
     # please insert magic here
     msg = json.loads(request.body)
     id = msg['message']['chat']['id']
-    fname = msg['message']["chat"]["first_name"]
-    message_text = msg['message']['text']
-
-    json_data = {
-        "chat_id" : id,
-        "text"  : "Wanna fight {}".format(fname)
-    }
-    requests.post(BOT_SEND_URL, json=json_data)
-    json_data["text"] = "imma whoop your ass!"
-    requests.post(BOT_SEND_URL, json=json_data)
+    name = msg['message']["chat"]["first_name"]
+    msg = msg['message']['text']
+    handle(id, name, msg)
     print("Telegram request was ", msg)
     return HttpResponse('OK')
 
-# def handle(data):
-#     msg = data["text"].lower()
-#     if msg == "iabpw":
-#         create_user_that_knows(id, name)
-#     else:
-#         create_user_that_doesnt(id, name)
+def handle(id, name, text):
+    msg = text.lower()
+    if msg == "iabpw":
+        create_user_that_knows(id, name)
+    else:
+        create_user_that_doesnt(id, name)
 
-# def check_user_exists(id):
-#     users =  
+def check_user_exists(id):
+    try:
+        user = User.objects.get(chat_id=id)
+        return True
+    except (User.DoesNotExist):
+        return False
 
-# def create_user_that_knows(id, name):
+def create_user_that_knows(id, name):
+    if (not check_user_exists(id)):
+        user = new User(chat_id=id, username = "name", knows= True)
+    send_message(id, "youre cool")
 
+def create_user_that_doesnt(id, name):
+    if (not check_user_exists(id)):
+        user = new User(chat_id=id, username = "name", knows= False)
+        send_message(id, "Sorry I dint want to talk to you!")
+    else:
+        send_message(id, "wanna fight {}?".format(name))
+        send_message(id, "Imma whoop your ass!!")
+
+def send_message(id, text):
+    data = {
+        "id": id,
+        "text": test 
+    }
+    requests.post(BOT_SEND_URL, json=data)
+        
 
     
     
